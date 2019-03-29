136e84558f1eeb1ef41ff47acc0f137388d2493d
Carlo <carlo.pezzotti@samtrevano.ch>
2019-03-29 16:20:05 +0100

    Jenkins commit

--- code/php/password/sendLostPassword.php
+++ code/php/password/sendLostPassword.php
@@ -4,7 +4,7 @@
 		if(isset($_POST["email"])){
 			User::tryEmail($_POST["email"]);
 			$cryptedMail = $_POST["email"] ^ "SGG<?rpF3FTebqx?(kgQR:hsq'mqZ!VH";
-			$message = 'Change password:<a href="http://localhost:8081/changePassword.php?id='.urlencode($cryptedMail).'">Click me!</a>';
+			$message = 'Change password:<a href="http://cashyland.tk/php/password/changePassword.php?id='.urlencode($cryptedMail).'">Click me!</a>';
 			$mailSender->mailSend($_POST["email"],"Reset password!",$message);
 			header("Location: ../../verifyMail.html");
 		}else{

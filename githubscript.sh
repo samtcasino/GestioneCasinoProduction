b566604b1b96b4e0e7b5c473e2ebe9ac2e036110
JenkinsCasino <tiakkadiubi@gmail.com>
2019-03-29 12:56:27 +0000

    Jenkins commit

--- githubscript.sh
+++ githubscript.sh
@@ -1,6 +1,6 @@
 cd /var/lib/jenkins/workspace
 shopt -s extglob
-cp !(.*) GestioneCasino/* GestioneCasinoProduction -rf
+cp !(.*) ./GestioneCasino/* ./GestioneCasinoProduction/ -rf
 cd GestioneCasinoProduction
 git remote set-url origin https://github.com/samtcasino/GestioneCasinoProduction.git
 git pull origin master --allow-unrelated-histories

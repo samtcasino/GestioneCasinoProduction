2c93c178325e4fc0f831cb0431634109a7d05078
Jenkins <jenkins@cashyland.tk>
2019-04-10 12:06:30 +0000

    Jenkins commit

--- code/php/tests/databaseCheck.php
+++ code/php/tests/databaseCheck.php
@@ -31,8 +31,8 @@ final class DatabaseTestCase extends TestCase
             "male",
             "Password&1"
         ));
-	    $result = $db->executeQuery("select email from user where email = 'a@a.ch'")["email"];
-        $this->assertTrue($result == "a@a.ch");
+	    $result = $db->executeQueryWithoutFetch("select email from user where email = 'a@a.ch'")->fetch();
+        $this->assertTrue($result["email"] == "a@a.ch");
 	    $db->executeQuery("delete from user where email = 'a@a.ch'");
     }
 
@@ -44,14 +44,14 @@ final class DatabaseTestCase extends TestCase
 
     public function testCorrectReturn():void{
         $db = new Database("127.0.0.1",3306,"cashyland","casinoAdmin","Casin02018"); 
-        $result = $db->executeQuery("select email,name from user where email = 'admin@admin.ch'");
-        $this->assertTrue(($result["email"] == "admin@admin.ch" && $result["name"] == "admin"));
+        $result = $db->executeQueryWithoutFetch("select email,name from user where email = 'admin'")->fetch();
+        $this->assertTrue(($result["email"] == "admin" && $result["name"] == "admin"));
     }
 
     public function testNotCorrectReturn():void{
         $db = new Database("127.0.0.1",3306,"cashyland","casinoAdmin","Casin02018"); 
-        $result = $db->executeQuery("select email,name from user where email = 'admin@admin.ch'");
-        $this->assertTrue(!($result["email"] == "adm@admin.ch" && $result["name"] == "ain"));
+        $result = $db->executeQueryWithoutFetch("select email,name from user where email = 'admin'")->fetch();
+        $this->assertTrue(!($result["email"] == "admin" && $result["name"] == "ain"));
     }
 
     public function testExecuteNotCorrectQuery(){
